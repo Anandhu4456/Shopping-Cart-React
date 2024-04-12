3e6cfeba16330746aae1267749260ddef4a76e2b
Anandhu4456 <anandhurajan56@gmail.com>
2024-04-13 00:10:15 +0530

    started with the navbar component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
