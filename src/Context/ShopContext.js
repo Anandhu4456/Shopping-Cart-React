2da4550a436bea3eac7a3fa73e91a54443ab46f2
Anandhu4456 <anandhurajan56@gmail.com>
2024-04-13 12:25:00 +0530

    added more products

---
+++ src/Context/ShopContext.js
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react'
+
+const ShopContext = () => {
+  
+  return (
+    <div>context</div>
+  )
+}
+
+export default ShopContext
--- src/Products.js
+++ src/Products.js
@@ -1,5 +1,10 @@
 import product1 from './assets/s23.webp'
 import product2 from './assets/iphone14.png'
+import product3 from './assets/canon-rf.webp'
+import product4 from './assets/puma.jpg'
+import product5 from './assets/casio.webp'
+import product6 from './assets/macbook.jpeg'
+
 
 export const PRODUCTS =  [
     {
@@ -13,5 +18,29 @@ export const PRODUCTS =  [
         productName : "I Phone",
         price : 80000,
         productImage :product2
+    },
+    {
+        id : 3,
+        productName : "canon-RF",
+        price : 80000,
+        productImage :product3
+    },
+    {
+        id : 4,
+        productName : "Puma",
+        price : 80000,
+        productImage :product4
+    },
+    {
+        id : 5,
+        productName : "Casio",
+        price : 80000,
+        productImage :product5
+    },
+    {
+        id : 6,
+        productName : "Macbook Air",
+        price : 80000,
+        productImage :product6
     }
 ]
