54b87cb1acfc6e6eecb535ec334107c1b0500eee
Anandhu4456 <anandhurajan56@gmail.com>
2024-04-13 12:45:38 +0530

    implemented add to cart and remove from cart logic in shop context

--- src/Context/ShopContext.js
+++ src/Context/ShopContext.js
@@ -1,10 +1,27 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
+import { PRODUCTS } from '../Products'
 
-const ShopContext = () => {
-  
-  return (
-    <div>context</div>
-  )
+export const ShopContext = createContext(null)
+
+const getDefaultCart = ()=>{
+  let cart = {}
+  for(let i = 1;i< PRODUCTS.length+1;i++){
+    cart[i] = 0
+  }
+  return cart
 }
 
-export default ShopContext
+export default function ShopContextProvider(props) {
+  const [cartItems,setCartItems] = useState(getDefaultCart())
+
+  const addToCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+  }
+
+  const removeFromCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]: prev[itemId]-1}))
+  }
+  return (
+    <ShopContextProvider>{props.children}</ShopContextProvider>
+  )
+}
